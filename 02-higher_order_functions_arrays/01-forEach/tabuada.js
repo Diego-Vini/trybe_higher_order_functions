c174aecb941a7e144acd2e8ac669998c175c7fdd
diego-vini <analistamm@gmail.com>
2021-08-30 17:11:17 -0300

    Ajustando código da tabuada

--- 02-higher_order_functions_arrays/01-forEach/tabuada.js
+++ 02-higher_order_functions_arrays/01-forEach/tabuada.js
@@ -1,13 +1,12 @@
 const indiceTabuada = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const imprimeTabuada = (indice) => {
+const imprimeTabuada = (item) => {
   for(let index = 0; index < indiceTabuada.length; index += 1) {
-    const resultado = indice * indiceTabuada[index]
-    console.log(`${indice} x ${indiceTabuada[index]}= ${resultado}`)
+    const resultado = item * indiceTabuada[index]
+    console.log(`${item} x ${indiceTabuada[index]}= ${resultado}`)
   }
-  
-}
+};
 
 indiceTabuada.forEach((item) => {
   imprimeTabuada(item)
-})
+});
